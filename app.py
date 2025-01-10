c258601c7612f736d2b0a920e1f69d78fc1691f8
Sakshi Raj <sakshiraj708@gmail.com>
2025-01-10 19:33:21 +0530

    third

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+# @app.route('/')
+# def home():
+#     return "Hello, Flask!"
 
 @app.route("/")
 def hello_world():
